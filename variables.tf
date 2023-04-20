2c7b2954001e6d2cf7dd6fa7ff33650e0df4f8e7
Mansab Uppal <mansab.uppal.extern@porsche.digital>
2023-04-20 14:05:43 +0200

    chore:  Update variable's nullable attribute to true

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {
   default = "eu-central-1"
   description = "Specify the AWS region for the CloudWatch Log Group containing CloudTrail Logs"
+  nullable = false
 }
 
 variable "aws_cloudwatch_cloudtrail_log_group" {
     default = "aws/cloudtrail"
     description = "Specify the cloudwatch log group which contains cloudtrail logs"
+    nullable = false
 }
